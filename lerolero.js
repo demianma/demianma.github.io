78117c52d8e06a241bd7045b384de2798be25735
Demian Andrade <tobata@gmail.com>
2018-04-18 18:19:25 -0300

    Create lerolero.js

---
+++ lerolero.js
@@ -0,0 +1,170 @@
+tab0 = new Array(
+			"Caros amigos, ",
+			"Por outro lado, ",
+			"Assim mesmo, ",
+			"No entanto, nÃ£o podemos esquecer que ",
+			"Do mesmo modo, ",
+			"A prÃ¡tica cotidiana prova que ",
+			"Nunca Ã© demais lembrar o peso e o significado destes problemas, uma vez que ",
+			"As experiÃªncias acumuladas demonstram que ",
+			"Acima de tudo, Ã© fundamental ressaltar que ",
+			"O incentivo ao avanÃ§o tecnolÃ³gico, assim como ",
+			"NÃ£o obstante, ",
+			"Todas estas questÃµes, devidamente ponderadas, levantam dÃºvidas sobre se ",
+			"Pensando mais a longo prazo, ",
+			"O que temos que ter sempre em mente Ã© que ",
+			"Ainda assim, existem dÃºvidas a respeito de como ",
+			"Gostaria de enfatizar que ",
+			"Todavia, ",
+			"A nÃ­vel organizacional, ",
+			"O empenho em analisar ",
+			"Percebemos, cada vez mais, que ",
+			"No mundo atual, ",
+			"Ã‰ importante questionar o quanto ",
+			"Neste sentido, ",
+			"Evidentemente, ",
+			"Por conseguinte, ",
+			"Ã‰ claro que ",
+			"Podemos jÃ¡ vislumbrar o modo pelo qual ",
+			"Desta maneira, ",
+			"O cuidado em identificar pontos crÃ­ticos n",
+			"A certificaÃ§Ã£o de metodologias que nos auxiliam a lidar com ");
+	tab1 = new Array(
+			"a execuÃ§Ã£o dos pontos do programa ",
+			"a complexidade dos estudos efetuados ",
+			"a contÃ­nua expansÃ£o de nossa atividade ",
+			"a estrutura atual da organizaÃ§Ã£o ",
+			"o novo modelo estrutural aqui preconizado ",
+			"o desenvolvimento contÃ­nuo de distintas formas de atuaÃ§Ã£o ",
+			"a constante divulgaÃ§Ã£o das informaÃ§Ãµes ",
+			"a consolidaÃ§Ã£o das estruturas ",
+			"a consulta aos diversos militantes ",
+			"o inÃ­cio da atividade geral de formaÃ§Ã£o de atitudes ",
+			"o desafiador cenÃ¡rio globalizado ",
+			"a mobilidade dos capitais internacionais ",
+			"o fenÃ´meno da Internet ",
+			"a hegemonia do ambiente polÃ­tico ",
+			"a expansÃ£o dos mercados mundiais ",
+			"o aumento do diÃ¡logo entre os diferentes setores produtivos ",
+			"a crescente influÃªncia da mÃ­dia ",
+			"a necessidade de renovaÃ§Ã£o processual ",
+			"a competitividade nas transaÃ§Ãµes comerciais ",
+			"o surgimento do comÃ©rcio virtual ",
+			"a revoluÃ§Ã£o dos costumes ",
+			"o acompanhamento das preferÃªncias de consumo ",
+			"o comprometimento entre as equipes ",
+			"a determinaÃ§Ã£o clara de objetivos ",
+			"a adoÃ§Ã£o de polÃ­ticas descentralizadoras ",
+			"a valorizaÃ§Ã£o de fatores subjetivos ",
+			"a percepÃ§Ã£o das dificuldades ",
+			"o entendimento das metas propostas ",
+			"o consenso sobre a necessidade de qualificaÃ§Ã£o ",
+			"o julgamento imparcial das eventualidades ");
+	tab2 = new Array(
+			"nos obriga Ã  anÃ¡lise ",
+			"cumpre um papel essencial na formulaÃ§Ã£o ",
+			"exige a precisÃ£o e a definiÃ§Ã£o ",
+			"auxilia a preparaÃ§Ã£o e a composiÃ§Ã£o ",
+			"garante a contribuiÃ§Ã£o de um grupo importante na determinaÃ§Ã£o ",
+			"assume importantes posiÃ§Ãµes no estabelecimento ",
+			"facilita a criaÃ§Ã£o ",
+			"obstaculiza a apreciaÃ§Ã£o da importÃ¢ncia ",
+			"oferece uma interessante oportunidade para verificaÃ§Ã£o ",
+			"acarreta um processo de reformulaÃ§Ã£o e modernizaÃ§Ã£o ",
+			"pode nos levar a considerar a reestruturaÃ§Ã£o ",
+			"representa uma abertura para a melhoria ",
+			"ainda nÃ£o demonstrou convincentemente que vai participar na mudanÃ§a ",
+			"talvez venha a ressaltar a relatividade ",
+			"prepara-nos para enfrentar situaÃ§Ãµes atÃ­picas decorrentes ",
+			"maximiza as possibilidades por conta ",
+			"desafia a capacidade de equalizaÃ§Ã£o ",
+			"agrega valor ao estabelecimento ",
+			"Ã© uma das consequÃªncias ",
+			"promove a alavancagem ",
+			"nÃ£o pode mais se dissociar ",
+			"possibilita uma melhor visÃ£o global ",
+			"estimula a padronizaÃ§Ã£o ",
+			"aponta para a melhoria ",
+			"faz parte de um processo de gerenciamento ",
+			"causa impacto indireto na reavaliaÃ§Ã£o ",
+			"apresenta tendÃªncias no sentido de aprovar a manutenÃ§Ã£o ",
+			"estende o alcance e a importÃ¢ncia ",
+			"deve passar por modificaÃ§Ãµes independentemente ",
+			"afeta positivamente a correta previsÃ£o ");
+	tab3 = new Array(
+			"das condiÃ§Ãµes financeiras e administrativas exigidas.",
+			"das diretrizes de desenvolvimento para o futuro.",
+			"do sistema de participaÃ§Ã£o geral.",
+			"das posturas dos Ã³rgÃ£os dirigentes com relaÃ§Ã£o Ã s suas atribuiÃ§Ãµes.",
+			"das novas proposiÃ§Ãµes.",
+			"das direÃ§Ãµes preferenciais no sentido do progresso.",
+			"do sistema de formaÃ§Ã£o de quadros que corresponde Ã s necessidades.",
+			"das condiÃ§Ãµes inegavelmente apropriadas.",
+			"dos Ã­ndices pretendidos.",
+			"das formas de aÃ§Ã£o.",
+			"dos paradigmas corporativos.",
+			"dos relacionamentos verticais entre as hierarquias.",
+			"do processo de comunicaÃ§Ã£o como um todo.",
+			"dos mÃ©todos utilizados na avaliaÃ§Ã£o de resultados.",
+			"de todos os recursos funcionais envolvidos.",
+			"dos nÃ­veis de motivaÃ§Ã£o departamental.",
+			"da gestÃ£o inovadora da qual fazemos parte.",
+			"dos modos de operaÃ§Ã£o convencionais.",
+			"de alternativas Ã s soluÃ§Ãµes ortodoxas.",
+			"dos procedimentos normalmente adotados.",
+			"dos conhecimentos estratÃ©gicos para atingir a excelÃªncia.",
+			"do fluxo de informaÃ§Ãµes.",
+			"do levantamento das variÃ¡veis envolvidas.",
+			"das diversas correntes de pensamento.",
+			"do impacto na agilidade decisÃ³ria.",
+			"das regras de conduta normativas.",
+			"do orÃ§amento setorial.",
+			"do retorno esperado a longo prazo.",
+			"do investimento em reciclagem tÃ©cnica.",
+			"do remanejamento dos quadros funcionais.");
+
+	//randomizer
+	function randomize(a) {
+		var i;
+		for (i = a.length; i--; ) {
+			var j = Math.floor((i + 1) * Math.random());
+			var temp = a[i];
+			a[i] = a[j];
+			a[j] = temp;
+		}
+		return a;
+	}
+	
+	//main
+	function leroLero() {
+		//get form entries
+		var title = document.getElementById("t_titulo").value;
+		var lines = document.getElementById("t_linhas").value;
+
+		//new randomized arrays
+		var r_tab0 = randomize(tab0);
+		var r_tab1 = randomize(tab1);
+		var r_tab2 = randomize(tab2);
+		var r_tab3 = randomize(tab3);
+
+		//build paragraphs
+		var paragraph = 0;
+		var text = "";
+		while (lines > 0) {
+			for (i = 0; i < r_tab0.length; i++) {
+				text += r_tab0[i] + r_tab1[i] + r_tab2[i] + r_tab3[i] + " ";
+				//randomize between 2 and 5 line paragraph and add break-line
+				if (++paragraph >= (2 + Math.ceil(Math.random() * 3))) {
+					text += "<br><br>";
+					paragraph = 0;
+				}
+				if (--lines <= 0) {
+					break;
+				}
+			}
+		}
+		//put a nice title
+		document.getElementById("titulo").innerHTML = title;
+		//put a wonderful bullshit
+		document.getElementById("texto").innerHTML = text;
+	}
